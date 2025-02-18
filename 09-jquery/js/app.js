650c2704f5046ed62b47537ea90b8d4017320fcd
isaacramirez-07 <ramirezlopezisaac.2002@gmail.com>
2025-02-18 17:18:14 -0500

    agregar, contar y eliminar

--- 09-jquery/js/app.js
+++ 09-jquery/js/app.js
@@ -1,18 +1,50 @@
-$(function(){
-    $('footer').on('click','#add',function(){
-        if($('#input-task').val().length > 0){
+$(function () {
+    //count task
+    countTasks()
+    countRemains()
+    //add task
+    $('footer').on('click', '#add', function () {
+        if ($('#input-task').val().length > 0) {
 
-            $task = '<article>\
-                        <input type="checkbox" >\
-                        <p>'+$('#input-task').val()+'</p>\
-                        <button>&times;</button>\
-                    </article>'
+            $task = '<article> \
+                <input type="checkbox"> \
+                    <p> '+ $('#input-task').val() + '</p> \
+                    <button>&times;</button> \
+            </article>'
             $('section.list').append($task)
             $('#input-task').val('')
-
-        }else{
+            countTasks()
+            countRemains()
+        } else {
             alert('Please! Enter a Task')
         }
     })
-})
+    //toggle task (remain/done)
 
+    $('body').on('click', 'input[type=checkbox]', function () {
+        //if checked
+        if ($(this).prop('checked')) {
+            $(this).parent().addClass('checked')
+        } else {
+            $(this).parent().removeClass('checked')
+        }
+        countRemains()
+    })
+    // remove task 
+    $('body'). on('click', 'article button', function () {
+        $(this).closest('article').remove()
+        countTasks()
+        countRemains()
+    })
+})
+// count tasks
+function countTasks() {
+    $('.num-tasks').text($('article').length)
+    $('.title-tasks').text($('article').length > 1 ? 'tasks' : 'task')
+}
+// count remains
+function countRemains() {
+    $remain = Math.abs($('.checked').length - $('article').length)
+    $('.num-remains').text($remain)
+    $('.title-remains').text($remain > 1 ? 'Remains' : 'Remain')
+}
