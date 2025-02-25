230b42796ba0885cc5430b7eb756fe26394f7551
isaacramirez-07 <ramirezlopezisaac.2002@gmail.com>
2025-02-25 16:09:39 -0500

    se agrego boton de eliminar

--- 09-jquery/js/app.js
+++ 09-jquery/js/app.js
@@ -1,8 +1,15 @@
 $(function () {
-    //count task
-    countTasks()
-    countRemains()
-    //add task
+    // Check LocalStorage
+    if (localStorage.getItem('todoList') != null) {
+        $('.list').html(localStorage.getItem('todoList'))
+        countTasks()
+        countRemains()
+    } else {
+        //count Tasks & Remains
+        countTasks()
+        countRemains()
+    }
+    //Add Task
     $('footer').on('click', '#add', function () {
         if ($('#input-task').val().length > 0) {
 
@@ -19,32 +26,46 @@ $(function () {
             alert('Please! Enter a Task')
         }
     })
-    //toggle task (remain/done)
-
+    //Toggle Task (Remain/Done)
     $('body').on('click', 'input[type=checkbox]', function () {
-        //if checked
+        //If checked
         if ($(this).prop('checked')) {
+            $(this).attr('checked', true)
             $(this).parent().addClass('checked')
         } else {
+            $(this).attr('checked', false)
             $(this).parent().removeClass('checked')
         }
         countRemains()
     })
-    // remove task 
-    $('body'). on('click', 'article button', function () {
+    //Remove Task
+    $('body').on('click', 'article button', function () {
         $(this).closest('article').remove()
         countTasks()
         countRemains()
     })
 })
-// count tasks
+//Count Tasks
 function countTasks() {
     $('.num-tasks').text($('article').length)
-    $('.title-tasks').text($('article').length > 1 ? 'tasks' : 'task')
+    $('.title-tasks').text($('article').length > 1 ? 'Tasks' : 'Task')
 }
-// count remains
+//Count Remains
 function countRemains() {
     $remain = Math.abs($('.checked').length - $('article').length)
     $('.num-remains').text($remain)
     $('.title-remains').text($remain > 1 ? 'Remains' : 'Remain')
+    //set LocalStorage
+        localStorage.setItem('todoList',$('.list').html())
 }
+
+function eliminar() {
+    $('.list').html('')
+    localStorage.removeItem('todolist')
+    countTasks()
+    countRemains()
+
+}
+$('body').on('click','#eliminar',function(){
+    eliminar()
+})
